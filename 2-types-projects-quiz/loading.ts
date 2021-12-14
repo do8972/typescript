2fcc5a13cee916555089fb18bf95cc0bda0d870d
do8972 <do8972@gmail.com>
2021-12-15 00:17:54 +0900

    2-types-projects-quiz

---
+++ 2-types-projects-quiz/loading.ts
@@ -0,0 +1,42 @@
+{
+  /**
+   * Print Loading State
+   */
+  type LoadingState = {
+    state: "loading";
+  };
+
+  type SuccessState = {
+    state: "success";
+    response: {
+      body: string;
+    };
+  };
+
+  type FailState = {
+    state: "fail";
+    reason: string;
+  };
+
+  type ResourceLoadState = LoadingState | SuccessState | FailState;
+
+  function printLoginStates(state: ResourceLoadState) {
+    switch (state.state) {
+      case "loading":
+        console.log("loading...");
+        break;
+      case "success":
+        console.log(`${state.response.body}`);
+        break;
+      case "fail":
+        console.log(`${state.reason}`);
+        break;
+      default:
+        throw new Error(`unknown state: ${state}`);
+    }
+  }
+
+  printLoginStates({ state: "loading" }); // 👀 loading...
+  printLoginStates({ state: "success", response: { body: "loaded" } }); // 😃 loaded
+  printLoginStates({ state: "fail", reason: "no network" }); // 😱 no network
+}
