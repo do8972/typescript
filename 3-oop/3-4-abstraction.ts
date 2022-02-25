668e6f5632f5f45aed09fc03711cc15bb431416e
do8972 <do8972@gmail.com>
2022-02-25 23:18:01 +0900

    3-4-abstraction.ts

---
+++ 3-oop/3-4-abstraction.ts
@@ -0,0 +1,104 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+
+  interface CoffeeMaker {
+    // 규약.
+    makeCoffee(shots: number): CoffeeCup;
+  }
+
+  interface CommercialCoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+    fillCoffeeBeans(beans: number): void;
+    clean(): void;
+  }
+
+  class CoffeeMachine implements CoffeeMaker, CommercialCoffeeMaker {
+    // implements => interface 규약을 따라야함. (규약에 맞게 클레스 사용(구현))
+    private static BEANS_GRAMM_PER_SHOT: number = 7;
+    private coffeeBeans: number = 0;
+
+    private constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    static makeMachine(coffeeBeans: number): CoffeeMachine {
+      return new CoffeeMachine(coffeeBeans);
+    }
+
+    fillCoffeeBeans(beans: number) {
+      if (beans < 0) {
+        throw new Error("value for beans should be greater than 0");
+      }
+      this.coffeeBeans += beans;
+    }
+
+    clean() {
+      console.log("cleaning the machine... 🧼");
+    }
+
+    private grindBeans(shots: number) {
+      console.log(`grinding beans for ${shots}`);
+      if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
+        throw new Error("Not enough coffee beans!");
+      }
+      this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
+    }
+
+    private preheat(): void {
+      console.log("heating up... 🔥");
+    }
+
+    private extract(shots: number): CoffeeCup {
+      console.log(`Pulling ${shots} shots... ☕️`);
+      return {
+        shots,
+        hasMilk: false,
+      };
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      this.grindBeans(shots);
+      this.preheat();
+      return this.extract(shots);
+    }
+  }
+
+  // const maker: CoffeeMachine = CoffeeMachine.makeMachine(32);
+  // 1. CoffeeMachine type으로 object를 받게 되면 class안에 있는 public을 전부 사용할 수 있지만,
+  // maker.fillCoffeeBeans(32);
+  // maker.makeCoffee(2);
+
+  // const maker2: CommercialCoffeeMaker = CoffeeMachine.makeMachine(32);
+  // 2. interface로 다시 type을 제한해서 받게되면 정의된 것들만 사용할 수 있음.
+  // maker2.fillCoffeeBeans(32);
+  // maker2.makeCoffee(2);
+  // maker2.clean();
+
+  class AmateurUser {
+    constructor(private machine: CoffeeMaker) {}
+    makeCoffee() {
+      const coffee = this.machine.makeCoffee(2);
+      console.log(coffee);
+    }
+  }
+
+  class ProBarista {
+    constructor(private machine: CommercialCoffeeMaker) {}
+    makeCoffee() {
+      const coffee = this.machine.makeCoffee(2);
+      console.log(coffee);
+      this.machine.fillCoffeeBeans(45);
+      this.machine.clean();
+    }
+  }
+
+  const maker: CoffeeMachine = CoffeeMachine.makeMachine(32);
+  const amateur = new AmateurUser(maker);
+  const pro = new ProBarista(maker);
+
+  amateur.makeCoffee();
+  pro.makeCoffee();
+}
