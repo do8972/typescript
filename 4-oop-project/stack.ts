347e66d6747b17746e76a6604c13acaace07ada9
do8972 <do8972@gmail.com>
2022-04-16 15:26:17 +0900

    stack.ts

---
+++ 4-oop-project/stack.ts
@@ -0,0 +1,52 @@
+// 연결리스트
+interface Stack {
+  readonly size: number;
+  push(value: string): void;
+  pop(): string;
+}
+
+type StackNode = {
+  readonly value: string;
+  readonly next?: StackNode;
+};
+
+class StackImpl implements Stack {
+  private _size: number = 0;
+  private head?: StackNode;
+
+  constructor(private capacity: number) {}
+
+  get size() {
+    return this._size;
+  }
+  push(value: string) {
+    if (this.size === this.capacity) {
+      throw new Error("Stack is full!");
+    }
+    const node: StackNode = {
+      value,
+      next: this.head,
+    };
+    this.head = node;
+    this._size++;
+    console.log(node.value);
+  }
+  pop(): string {
+    // null == undefined 두가지를 다 걸러주기 위해. 하지만 null !== undefined
+    if (this.head == null) {
+      throw new Error("Stack is empty!");
+    }
+    const node = this.head;
+    this.head = node.next;
+    this._size--;
+    return node.value;
+  }
+}
+
+const stack = new StackImpl(10);
+stack.push("Steve 1");
+stack.push("Bob 2");
+stack.push("Jun 3");
+while (stack.size !== 0) {
+  console.log(stack.pop());
+}
