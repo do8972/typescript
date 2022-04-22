dbb88b0c508b15d97ee4386266f37229e9c62f33
do8972 <do8972@gmail.com>
2022-04-22 23:48:11 +0900

    8-1-handling.ts

---
+++ 8-exception/8-1-handling.ts
@@ -0,0 +1,42 @@
+class TimeoutError extends Error {}
+class OfflineError extends Error {}
+
+class NetworkClient {
+  tryConnect(): void {
+    throw new Error("no network");
+  }
+}
+
+class UserService {
+  constructor(private clinet: NetworkClient) {}
+
+  login() {
+    this.clinet.tryConnect();
+    // 정확하게 에러를 잡지 못할거면 try catch 사용X
+    // try {
+    //   this.clinet.tryConnect();
+    // } catch (error) {
+    //   console.log("catched!");
+    // }
+    // login...
+  }
+}
+
+const client = new NetworkClient();
+const service = new UserService(client);
+// service.login();
+
+class App {
+  constructor(private userService: UserService) {}
+  run() {
+    // 가장 정확하게 에러 핸들링을 할 수 있는 곳에 작성해야 함.
+    try {
+      this.userService.login();
+    } catch (error) {
+      // show dialog to user
+    }
+  }
+}
+
+const app = new App(service);
+app.run();
